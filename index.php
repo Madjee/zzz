d7f74519a390530786a2697f291ba7873d3d254f
Madjee <kiritork9@gmail.com>
2023-10-26 14:44:01 +0200

    adeee

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    // LES VARIABLES 
+    $chaine= "coucou";
+
+    $nombre = 5; 
+    $tab = array(5, "test"); 
+    
+    //Afficher des variables 
+    echo $chaine.'<br>';
+    echo $nombre;
+
+    $prenom = "Maxime";
+    $nom = "BLOQUE";
+
+    echo $prenom;
+    echo $nom;
+    echo '<br> <br>';
+    
+    //LES CONCATENATIONS
+
+    $fullname = $prenom.' '.$nom;
+    echo $fullname;
+
+    echo '<br> <br>';
+    
+    echo $prenom.' '.$nom;
+
+?>
